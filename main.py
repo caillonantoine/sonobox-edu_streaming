18bb76e69ced128ad88e5505b4fc15ce5a3842bb
Antoine Caillon <caillonantoine@gmail.com>
2017-11-09 11:52:32 +0100

    Modification du scale midi et suppression des print en trop

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from ext import musescore as ms
 from ext import soundcard as sc
 
+#%%
+
 def pad(array):
     if len(array) > 2**16:
         print "TROP GRAND"
@@ -15,23 +17,23 @@ def pad(array):
 def get_midi_note_from_f(f):
     midi_scale = 110*np.power(2,np.array(range(120))/12.)
     midi_note = np.array(range(120))+45
-    return midi_note[np.argmin(abs(midi_scale - f))]
+    return midi_note[np.argmin(abs(midi_scale - f))]-12
         
 def analyse():
     signal_in = sc.init_sound_card(4096)
     recorded_frequency = []
     for elm in signal_in:
         s_ = np.fft.rfft(pad(elm))
-        f,A = np.argmax(abs(s_)),np.max(abs(s_))
+        f,A = np.argmax(abs(s_)[1050:])+1050,np.max(abs(s_))
         f,A = f*44100/float(2**16),20*np.log10(2*A/4096.)
         
         if A>=-35:   
-            result = "Fréquence: {} Hz, Amplitude: {} dBFS".format(int(f),int(A))
-            print result
+            #result = "Fréquence: {} Hz, Amplitude: {} dBFS".format(int(f),int(A))
+            #print result
             recorded_frequency.append(f)
         else:
             if recorded_frequency:
-                ms.muse.send(get_midi_note_from_f(np.mean(recorded_frequency)/3.))
+                ms.muse.send(get_midi_note_from_f(np.mean(recorded_frequency)))
                 recorded_frequency = []
             
             
