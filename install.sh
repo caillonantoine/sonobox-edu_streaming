bdfa2c1f9881c3c7a5cdfd1340c991bc572575c8
Antoine Caillon <caillonantoine@gmail.com>
2017-11-11 11:01:26 +0000

    Ajout de musescore

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt-get install python-pip spyder libasound2-dev libjack-dev
+apt-get install python-pip spyder libasound2-dev libjack-dev musescore
 wget https://pypi.python.org/packages/52/b6/44871791929d9d7e11325af0b7be711388dfeeab17147988f044a41a6d83/pyalsaaudio-0.8.4.tar.gz
 tar xvzf pyalsaaudio-0.8.4.tar.gz
 cd pyalsaaudio-0.8.4/
