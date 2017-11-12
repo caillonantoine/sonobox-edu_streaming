c42db46f5206190cf6094ae918d3b66e466624db
Antoine Caillon <caillonantoine@gmail.com>
2017-11-12 15:51:35 +0000

    Suppression de Musescore, matplotlib dans l'install

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-apt-get install python-pip spyder libasound2-dev libjack-dev musescore
+apt-get install python-pip libasound2-dev libjack-dev
 wget https://pypi.python.org/packages/52/b6/44871791929d9d7e11325af0b7be711388dfeeab17147988f044a41a6d83/pyalsaaudio-0.8.4.tar.gz
 tar xvzf pyalsaaudio-0.8.4.tar.gz
 cd pyalsaaudio-0.8.4/
 python setup.py install
-pip install mido python-rtmidi numpy matplotlib scipy
+pip install mido python-rtmidi numpy scipy
 cd ..
 rm -r pyalsaaudio-0.8.4/ pyalsaaudio-0.8.4.tar.gz
